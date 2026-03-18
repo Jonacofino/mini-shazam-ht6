62e5df66f27fde60195eb89289af3c7d2b4257b9
Jonacofino <cofinojonathan1@gmail.com>
2026-03-18 11:48:01 -0600

    mini shazam

--- src/hash_table.py
+++ src/hash_table.py
@@ -65,7 +65,8 @@ class HashTable:
         Returns:
             An integer bucket index in [0, self._capacity)
         """
-        raise NotImplementedError("_hash not implemented yet.")
+        return int(key * 2654435761) & self.capacity
+
 
     # ------------------------------------------------------------------ #
     # Core operations
@@ -89,7 +90,13 @@ class HashTable:
             key: The hash key (an integer)
             value: The value to store (in our case, a (song_id, time_offset) tuple)
         """
-        raise NotImplementedError("insert not implemented yet.")
+
+        index = self._hash(key)
+        self._buckets[index].append((key,value))
+        self._size +=1
+
+        if self.load_factor()>9.75:
+            self._resize()
 
     def lookup(self, key):
         """
@@ -111,7 +118,12 @@ class HashTable:
         Returns:
             A list of values associated with this key (may be empty)
         """
-        raise NotImplementedError("lookup not implemented yet.")
+        index = self._hash(key)
+        values = []
+        for f_key, value in self._buckets[index]:
+            if key == f_key:
+                values.append(value)
+        return values
 
     # ------------------------------------------------------------------ #
     # Size & statistics
@@ -119,11 +131,11 @@ class HashTable:
 
     def size(self):
         """Return the total number of stored entries."""
-        raise NotImplementedError("size not implemented yet.")
+        return self._size
 
     def capacity(self):
         """Return the current number of buckets."""
-        raise NotImplementedError("capacity not implemented yet.")
+        return self._capacity
 
     def load_factor(self):
         """
@@ -137,7 +149,8 @@ class HashTable:
 
         We resize when this exceeds 0.75 to keep lookups fast.
         """
-        raise NotImplementedError("load_factor not implemented yet.")
+        if self._capacity == 0:
+            return 0
 
     def stats(self):
         """
@@ -156,7 +169,32 @@ class HashTable:
         Returns:
             dict with the keys described above
         """
-        raise NotImplementedError("stats not implemented yet.")
+
+        empty_buckets = 0
+        max_chain_length = 0
+        avg_chain_length = 0
+
+        for bucket in self._buckets:
+            if len(bucket) == 0:
+                empty_buckets += 1
+            max_chain_length = max(max_chain_length, len(bucket))
+            avg_chain_length += len(bucket)
+
+
+
+
+        return {
+            "capacity": self._capacity,
+            "size": self._size,
+            "load_factor": self.load_factor(),
+            "empty_buckets": empty_buckets,
+            "max_chain_length": max_chain_length,
+            "avg_chain_length": round(avg_chain_length/self._capacity, 4),
+        }
+
+
+
+
 
     # ------------------------------------------------------------------ #
     # Resizing
@@ -184,7 +222,18 @@ class HashTable:
         Returns:
             The smallest prime >= n
         """
-        raise NotImplementedError("_next_prime not implemented yet.")
+        if n < 2:
+            return 2
+        
+        prime = n
+        if prime % 2 == 0:
+             prime +=1
+
+        for i in range(3, math.floor(math.sqrt(prime))):
+            if prime % i == 0:
+                prime +=2
+        
+        return prime
 
     def _resize(self):
         """
@@ -203,4 +252,13 @@ class HashTable:
         What is the time complexity of this operation? How often does it
         happen? What is the amortized cost per insertion? (Think about this!)
         """
-        raise NotImplementedError("_resize not implemented yet.")
+        new_capacity = self._next_prime(self._capacity * 2)
+        old_buckets = self._buckets
+
+        self._capacity = new_capacity
+        self._size = 0
+        self._buckets = [[] for _ in range(self._capacity)]
+
+        for bucket in old_buckets:
+            for key, value in bucket:
+                self.insert(key, value)
